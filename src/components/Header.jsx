4d174be9245591d0411d9f5a817ec944be3553e1
Indra Pranata <indrapranata120@gmail.com>
2022-05-11 11:42:51 +0800

    Create Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { AppBar, Toolbar, Typography as Text } from '@mui/material';
+
+const Header = () => (
+  <AppBar>
+    <Toolbar>
+      <Text variant='h5'>My Tasks</Text>
+    </Toolbar>
+  </AppBar>
+);
+
+export default Header;
